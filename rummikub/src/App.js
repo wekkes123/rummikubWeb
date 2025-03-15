880374aeefbd2cc86ddf9d1b06b42baab7e1411d
wekkes123 <vanloo.wout@gmail.com>
2025-03-15 17:27:55 +0100

    added a frontpage with colours from prototype and working language switching

--- rummikub/src/App.js
+++ rummikub/src/App.js
@@ -2,6 +2,7 @@ import { React } from 'react';
 import { useTranslation } from "react-i18next";
 import "./i18n";
 //import SeededRandomGenerator from './Components/SeededRNG';
+import Frontpage from "./Pages/Frontpage"
 
 function App() {
     const { t, i18n } = useTranslation();
@@ -11,13 +12,7 @@ function App() {
     };
 
     return (
-        <div style={{ textAlign: "center", marginTop: "50px" }}>
-            yo
-            <h1>{t("welcome")}</h1>
-            <p>{t("description")}</p>
-            <button onClick={() => changeLanguage("en")}>English</button>
-            <button onClick={() => changeLanguage("nl")}>Nederlands</button>
-        </div>
+        <Frontpage/>
     );
 }
 
---
+++ rummikub/src/Pages/Frontpage.js
@@ -0,0 +1,163 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { Layout, Button, Typography, Space, ConfigProvider } from 'antd';
+import { PlayCircleOutlined, ReadOutlined, SettingOutlined } from '@ant-design/icons';
+import ukFlag from '../images/Flag_of_the_United_Kingdom.png';
+import nlFlag from '../images/Flag_of_Belgium.png';
+
+const { Content } = Layout;
+const { Title } = Typography;
+
+const FrontPage = () => {
+    const { t, i18n } = useTranslation();
+
+    const changeLanguage = (lang) => {
+        i18n.changeLanguage(lang);
+    };
+
+    // Get the full language name
+    const getLanguageName = (code) => {
+        switch(code) {
+            case 'en': return 'English';
+            case 'nl': return 'Nederlands';
+            default: return code;
+        }
+    };
+
+    return (
+        <ConfigProvider
+            theme={{
+                token: {
+                    colorPrimary: '#FFB703',
+                    borderRadius: 8,
+                },
+            }}
+        >
+            <Layout style={{ minHeight: '100vh', backgroundColor: '#58B4D1', position: 'relative' }}>
+                {/* Language buttons in top left */}
+                <Space
+                    style={{
+                        position: 'absolute',
+                        top: 20,
+                        left: 20,
+                        zIndex: 1
+                    }}
+                >
+                    <Button
+                        onClick={() => changeLanguage('nl')}
+                        style={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            height: 'auto',
+                            padding: '8px',
+                            backgroundColor: i18n.language === 'nl' ? '#e6f7ff' : undefined,
+                            borderColor: i18n.language === 'nl' ? '#FFB703' : undefined,
+                            borderWidth: '2px',
+                            borderStyle: 'solid'
+                        }}
+                    >
+                        <img
+                            src={nlFlag}
+                            alt="Nederlands"
+                            style={{height: 40, marginBottom: 4}}
+                        />
+                        <span style={{fontSize: 16}}>Nederlands</span>
+                    </Button>
+
+                    <Button
+                        onClick={() => changeLanguage('en')}
+                        style={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            height: 'auto',
+                            padding: '8px',
+                            backgroundColor: i18n.language === 'en' ? '#e6f7ff' : undefined,
+                            borderColor: i18n.language === 'en' ? '#FFB703' : undefined,
+                            borderWidth: '2px',
+                            borderStyle: 'solid'
+                        }}
+                    >
+                        <img
+                            src={ukFlag}
+                            alt="English"
+                            style={{height: 40, marginBottom: 4}}
+                        />
+                        <span style={{fontSize: 16}}>English</span>
+                    </Button>
+                </Space>
+
+                <Content style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    padding: '50px 16px',
+                    marginBottom: '20vh',
+                }}>
+                    <Title style={{
+                        fontSize: 48,
+                        marginBottom: 60,
+                        color: 'black',
+                        fontWeight: 'bold',
+                        textAlign: 'center'
+                    }}>
+                        Rummikub
+                    </Title>
+
+                    <Space direction="vertical" size="large" style={{ width: 240 }}>
+                        <Button
+                            type="primary"
+                            size="large"
+                            icon={<PlayCircleOutlined />}
+                            block
+                            style={{
+                                height: 50,
+                                fontSize: 18,
+                                fontWeight: 'bold',
+                                color: 'black'
+                            }}
+                        >
+                            {t('start')}
+                        </Button>
+
+                        <Button
+                            type="primary"
+                            size="large"
+                            icon={<ReadOutlined />}
+                            block
+                            style={{
+                                height: 50,
+                                fontSize: 18,
+                                fontWeight: 'bold',
+                                color: 'black',
+                            }}
+                        >
+                            {t('tutorial')}
+                        </Button>
+
+                        <Button
+                            type="primary"
+                            size="large"
+                            icon={<SettingOutlined />}
+                            block
+                            style={{
+                                height: 50,
+                                fontSize: 18,
+                                fontWeight: 'bold',
+                                color: 'black',
+                            }}
+                        >
+                            {t('settings')}
+                        </Button>
+                    </Space>
+                </Content>
+            </Layout>
+        </ConfigProvider>
+    );
+};
+
+export default FrontPage;
