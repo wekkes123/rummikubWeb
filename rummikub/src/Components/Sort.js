5bd8d1cb0920124b5f671b480ecb9d82760bf181
Niels Coopmans <nielscoopmans@gmail.com>
2025-03-22 11:06:20 +0100

    sort function with joker

--- rummikub/src/Components/Sort.js
+++ rummikub/src/Components/Sort.js
@@ -1,7 +1,14 @@
-import React, { useMemo } from 'react';
+import { useMemo } from 'react';
 
 const sortTiles = (tiles, sortBy) => {
     return [...tiles].sort((a, b) => {
+        const isJokerA = a.value === "Joker";
+        const isJokerB = b.value === "Joker";
+
+        // Always move jokers to the end
+        if (isJokerA && !isJokerB) return 1;
+        if (!isJokerA && isJokerB) return -1;
+
         if (sortBy === "color") {
             return a.color.localeCompare(b.color) || a.value - b.value;
         } else {
@@ -11,9 +18,7 @@ const sortTiles = (tiles, sortBy) => {
 };
 
 const TileSorter = ({ tiles, sortby }) => {
-    const sortedTiles = useMemo(() => sortTiles(tiles, sortby), [tiles, sortby]);
-
-    return sortedTiles;
+    return useMemo(() => sortTiles(tiles, sortby), [tiles, sortby]);
 };
 
 export default TileSorter;
