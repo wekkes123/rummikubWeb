9510a437de750ace37ce6038e65c31bf154a0753
wekkes123 <vanloo.wout@gmail.com>
2025-03-29 12:54:00 +0100

    changed pile button

--- rummikub/src/Components/PileButton.js
+++ rummikub/src/Components/PileButton.js
@@ -1,29 +1,20 @@
 import React from 'react';
 import '../css/button.css';
 import { useTranslation } from 'react-i18next';
-import pile from '../images/pile-bag.png'
+import pile from '../images/draw-pile.png';
 
 const PileButton = ({
-                         onClick,
-                         className = '',
-                         disabled = false
-                     }) => {
-    const { t, i18n } = useTranslation();
+                        onClick,
+                        className = '',
+                        disabled = false
+                    }) => {
+    const { t } = useTranslation();
     const combinedClassName = `pile-button ${className} ${disabled ? 'disabled' : ''}`;
+
     return (
-        <button
-            onClick={onClick}
-            disabled={disabled}
-            className={combinedClassName}
-        >
-            <div className="take-tile">
-                {t('pile')}
-            </div>
-        <img
-            src = {pile}
-            alt = "Pick tile from pile"
-            className="pile-button-img"
-        />
+        <button onClick={onClick} disabled={disabled} className={combinedClassName}>
+            <img src={pile} alt="Pick tile from pile" className="pile-button-img" />
+            <div className="take-tile">{t('pile')}</div>
         </button>
     );
 };
