18ecd625066a3128e4f9c3c83aa71d773266e21d
wekkes123 <vanloo.wout@gmail.com>
2025-04-16 15:20:33 +0200

    deleted comments

--- final/src/App.js
+++ final/src/App.js
@@ -217,7 +217,6 @@ function App() {
                   );
                 }
 
-                // Only update the board AFTER the animation
                 const boardCopy = JSON.parse(JSON.stringify(newBoard));
                 boardCopy[j][k][m] = tile;
 
@@ -258,7 +257,6 @@ function App() {
                 break;
               }
             }
-            // For joker, we need to check if the jokerValue position is available
             else {
               const index = jokerValue - 1;  // Use jokerValue to determine position
               if (currentArray[index] === '1' || currentArray[index] === 1) {
@@ -280,7 +278,6 @@ function App() {
           for (const tile of moveData) {
             const [, tileNumber] = tile.split('-');
 
-            // Determine the correct index based on whether it's a joker or not
             let index;
             if (tileNumber !== 'j') {
               index = parseInt(tileNumber) - 1;
@@ -302,24 +299,20 @@ function App() {
               );
             }
 
-            // Store the appropriate value based on tile type
             if (tileNumber !== 'j') {
               targetArray[index] = 1;
             } else {
-              targetArray[index] = tile; // Store joker value
+              targetArray[index] = tile;
             }
 
-            // Only update the board AFTER the animation
             const boardCopy = JSON.parse(JSON.stringify(newBoard));
 
-            // Update the copy with the appropriate value
             if (tileNumber !== 'j') {
               boardCopy[2][startIndex + targetArrayIndex][index] = 1;
             } else {
               boardCopy[2][startIndex + targetArrayIndex][index] = tile;
             }
 
-            // Handle tile removal from computer rack
             const indexToRemove = tileNumber === 'j'
                 ? boardCopy[4].findIndex(t => t === '1-j' || t === '4-j')
                 : boardCopy[4].indexOf(tile);
@@ -331,9 +324,6 @@ function App() {
             newBoard = boardCopy;
             setBoard(boardCopy);
           }
-
-          // This line is redundant since we're already updating the board in the loop above
-          // newBoard[2][startIndex + targetArrayIndex] = targetArray;
         }
       }
     }
