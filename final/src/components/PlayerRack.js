0a4a4a06dd15eec652299ae6f7fd737da2067f32
nielscoopmans <nielscoopmans@gmail.com>
2025-06-17 11:06:29 +0200

    apple sound fix

--- final/src/components/PlayerRack.js
+++ final/src/components/PlayerRack.js
@@ -55,7 +55,7 @@ function PlayerRack({ playerhand, onDragEnd, tilesAreDraggable = true, setBoard,
         }
         setBoard(newBoard);
         setFirstTurnBoard(firstTurnBoard);
-        placeTileAudio.play();
+        //placeTileAudio.play();
     };
 
     const handleTileDragEnd = (item) => {
